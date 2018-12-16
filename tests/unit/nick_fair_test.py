266bdcfe90cf50fa5dd245ba2efd5f37d0b0c3fd
Stuart <stuart.a.jenkins@gmail.com>
2018-12-16 18:26:46 +0000

    update tests for nick fair code

--- tests/unit/nick_fair_test.py
+++ tests/unit/nick_fair_test.py
@@ -4,23 +4,13 @@ from U_FaIR.fiveFAIR import *
 
 from U_FaIR.testFAIR import *
 
-def test_hfc_impulse_response():
-    time = np.array([0, 1, 2, 3])
-    input_emissions = np.array([10, 0, 0, 0])
-    expected = 10 * np.exp(-time)
-
-    result = calculate_hfc_conc(input_emissions, time, lifetime=1.0)
-
-    # assert result == expected
-    np.testing.assert_allclose(result, expected)
-
 
 def test_oxfair_impulse_response_to_carbon():
     time = np.array([0, 1, 2, 3])
     input_emissions = np.array([[10, 0, 0, 0],[0,0,0,0],[0,0,0,0]])
 
-    Cexpected,RFexpected,Texpected = oxfair_test(input_emissions, time, lifetime=1.0)
-    Cresult,RFresult,Tresult = oxfair(input_emissions, time, lifetime=1.0)
+    Cexpected,RFexpected,Texpected = oxfair_test(input_emissions)
+    Cresult,RFresult,Tresult = oxfair(input_emissions)
 
     # assert result == expected
     np.testing.assert_allclose(Cresult, Cexpected)
