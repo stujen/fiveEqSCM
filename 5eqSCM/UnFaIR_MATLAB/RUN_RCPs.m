0ee8aac645e40093d7a5dbaa32ca7cbd7a67a81f
njleach <njleach.1@gmail.com>
2019-02-26 17:10:00 +0000

    Working Matlab model

--- 5eqSCM/UnFaIR_MATLAB/RUN_RCPs.m
+++ 5eqSCM/UnFaIR_MATLAB/RUN_RCPs.m
@@ -1,3 +1,43 @@
-m = csvread('/home/nleach/Documents/UnFaIR/5eqSCM/RCP_data/RCP6_EMISSIONS.csv', 37);
-emissions = zeros(3,length(m));
-emissions(
+RCP = input ('choose RCP (3PD,45,6,85)');
+
+ems = csvread(strcat('/home/nleach/Documents/UnFaIR/5eqSCM/RCP_data/RCP',RCP,'_EMISSIONS.csv'), 37);
+emissions = zeros(length(ems),3);
+emissions(:,1) = sum(ems(:,2:3),2);
+emissions(:,2:3) = ems(:,4:5);
+
+forc = csvread(strcat('/home/nleach/Documents/UnFaIR/5eqSCM/RCP_data/RCP',RCP,'_MIDYEAR_RADFORCING.csv'), 59);
+otherforc = forc(:,2) - forc(:,8);
+
+[C,RF,T,alpha] = UnFaIR(emissions,otherforc);
+
+plot_var = input( 'choose variable to plot (C, RF, T, alpha)' );
+
+if or(strcmp(plot_var,'C'),strcmp(plot_var,'alpha'))
+    plot_gas = input( 'choose gas to plot (CO2, CH4, N2O)' );
+end
+
+if strcmp(plot_var,'C')
+    if strcmp(plot_gas , 'CO2')
+        plot(C(:,1))
+    elseif strcmp(plot_gas , 'CH4')
+        plot(C(:,2))
+    elseif strcmp(plot_gas , 'N2O')
+        plot(C(:,3))
+    end
+    
+elseif strcmp(plot_var,'alpha')
+    if strcmp(plot_gas , 'CO2')
+        plot(alpha(:,1))
+    elseif strcmp(plot_gas , 'CH4')
+        plot(alpha(:,2))
+    elseif strcmp(plot_gas , 'N2O')
+        plot(alpha(:,3))
+    end
+
+elseif strcmp(plot_var,'T')
+    plot(T)
+    
+elseif strcmp(plot_var,'RF')
+    plot(RF)
+end 
+    
--- 5eqSCM/UnFaIR_MATLAB/UnFaIR.m
+++ 5eqSCM/UnFaIR_MATLAB/UnFaIR.m
@@ -16,7 +16,7 @@ emis2conc = gas_params(14,:);
 f = forcing_params;
 
 d = thermal_params(1,:);
-q = thermal_params(1,:);
+q = thermal_params(2,:);
 
 G = cumsum(emissions);
 C = zeros(size(emissions));
@@ -33,15 +33,19 @@ else
     F_ext = ones(length(emissions),1) .* F_ext;
 end
 
-alpha(1,:) = alpha_val(0,0,0,a,tau,r);
+alpha(1,:) = alpha_val(0.,0.,0.,a,tau,r);
 [C(1,:),R,G_A] = step_conc(zeros(4,3) , emissions(1,:) , alpha(1,:) , a , tau , PI_conc , emis2conc);
 RF(1,:) = step_forc(C(1,:) , PI_conc , f);
 [S,T(1)] = step_temp(zeros(1,2),sum(RF(1,:))+F_ext(1,:),q,d);
 
-for t = 2:(length(emissions)-1)
+for t = 2:(length(emissions))
     alpha(t,:) = alpha_val(G(t-1,:),G_A,T(t-1),a,tau,r);
     [C(t,:),R,G_A] = step_conc(R , emissions(t,:) , alpha(t,:) , a , tau , PI_conc , emis2conc);
     RF(t,:) = step_forc(C(t,:) , PI_conc , f);
     [S,T(t)] = step_temp(S,sum(RF(t,:))+F_ext(t,:),q,d);
+end
 
+RF = horzcat(RF,F_ext);
+RF = horzcat(RF,sum(RF,2));
+    
 end
