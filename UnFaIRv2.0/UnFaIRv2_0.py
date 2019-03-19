485b9c97a8fb5c88f2c28a19e013d00f5ecabc0a
Stuart <stuart.a.jenkins@gmail.com>
2019-03-19 17:05:50 +0000

    start making full code

--- UnFaIRv2.0/UnFaIRv2_0.py
+++ UnFaIRv2.0/UnFaIRv2_0.py
@@ -16,5 +16,23 @@
 
 # 5 - format output in nice way, connecting emissions, C, RF, T and parameter set used in nice way 
 
+import numpy as np
+import pandas as pd
+
+def return_empty_emissions(start_year=1765, end_year=2500, scen_names=[0]):
+
+	df = pd.DataFrame({'CO2':np.zeros(end_year+1-start_year),'CH4':np.zeros(end_year+1-start_year),'N2O':np.zeros(end_year+1-start_year)}, index=np.arange(start_year,end_year+1))
+
+	return pd.concat([df]*len(scen_names), keys=scen_names, axis=1)
+
+def input_to_numpy(input_df):
+
+	return input_df.values.reshape(input_df.columns.levels[0].size, input_df.columns.levels[1].size, input_df.index.size)
 
 
+input_emissions = return_empty_emissions(1800,1820,np.arange(1,11))
+
+input_numpy = input_to_numpy(input_emissions)
+
+print(input_emissions)
+print(input_numpy.shape)
